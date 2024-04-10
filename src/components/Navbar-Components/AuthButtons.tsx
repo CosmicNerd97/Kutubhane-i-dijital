a66468c0ade309900b5a382d74cee7b3b49e2623
Yuwen-0 <halil.alagul0@gmail.com>
2024-04-10 17:52:36 +0300

    yeah forget to save

--- src/components/Navbar-Components/AuthButtons.tsx
+++ src/components/Navbar-Components/AuthButtons.tsx
@@ -1,7 +1,7 @@
 /** @format */
 "use client";
 import { Flex } from "@radix-ui/themes";
-import AuthButton from "../AuthButton";
+import AuthButton from "./AuthButton";
 
 const AuthButtons = () => (
   <Flex gap="4" style={{ marginLeft: "auto" }}>
