c0210e108f79a06dbfcba6a479c336b8f36cd607
Yuwen-0 <halil.alagul0@gmail.com>
2024-04-16 07:06:24 +0300

    add:made book slides into 3d

--- src/app/api/fetchBooks/route.ts
+++ src/app/api/fetchBooks/route.ts
@@ -4,9 +4,10 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   const res = await fetch(
-    "https://openlibrary.org/search.json?q=Harry+Potter&lang=eng&sort=readinglog&limit=5&fields=title,cover_edition_key,author_name"
+    "https://openlibrary.org/search.json?q=Harry+Potter&lang=eng&sort=readinglog&limit=10&fields=title,cover_edition_key,author_name,number_of_pages_median"
   );
   const data = await res.json();
-  return NextResponse.json(data.docs);
+  const books = data.docs.filter((book: any) => book.title.includes("Harry"));
+  return NextResponse.json(books);
   //get the cover edition id and its gonnna give the current cover of the book
 };
--- src/components/BookSlides.tsx
+++ src/components/BookSlides.tsx
@@ -4,6 +4,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import { BookCover } from "book-cover-3d";
 import { setBooksOfTheWeek } from "@/store/booksSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -42,6 +43,7 @@ const BookSlides = ({ Callback }: any) => {
         maxWidth: "1400px",
         maxHeight: "600px",
         cursor: "grab",
+        paddingBlock: "40px",
       }}
       modules={[Navigation, Pagination, Scrollbar, Autoplay, A11y, Controller]}
       spaceBetween={0}
@@ -63,13 +65,21 @@ const BookSlides = ({ Callback }: any) => {
             paddingBottom: "30px",
           }}
         >
-          <Image
+          <BookCover
             width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-            src={`https://covers.openlibrary.org/b/olid/${cover_edition_key}-L.jpg`}
-            alt="Book Cover"
-          />
+            height={300}
+            rotate={25}
+            transitionDuration={0.3}
+            thickness={books.number_of_pages_median}
+          >
+            <Image
+              width={200}
+              height={200}
+              style={{ width: "auto", height: "auto" }}
+              src={`https://covers.openlibrary.org/b/olid/${cover_edition_key}-L.jpg`}
+              alt="Book Cover"
+            />
+          </BookCover>
         </SwiperSlide>
       ))}
     </Swiper>
